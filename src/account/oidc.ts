1d592bca54e38693f455734182e50ba7e4a7d4fc
Mid1i <michac.tchernov@yandex.ru>
2024-11-23 23:29:03 +0300

    feat: Добавлен конфиг для взаимодействия с REST API кейклока

---
+++ src/account/oidc.ts
@@ -0,0 +1,22 @@
+import { createMockReactOidc } from "oidc-spa/mock/react";
+import { createReactOidc } from "oidc-spa/react";
+
+
+const publicUrl = undefined;
+const isAuthGloballyRequired = true;
+
+
+export const realm = import.meta.env.DEV ? "" : window.location.pathname.split("/")[2];
+
+export const { OidcProvider, useOidc, getOidc } = import.meta.env.DEV
+    ? createMockReactOidc({
+          isUserInitiallyLoggedIn: true,
+          publicUrl,
+          isAuthGloballyRequired
+      })
+    : createReactOidc({
+          issuerUri: `${window.location.origin}/realms/${realm}`,
+          clientId: "account-console",
+          publicUrl,
+          isAuthGloballyRequired
+      });
